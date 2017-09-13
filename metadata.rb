62aeb63beb7d2902c8ead09751db8e81b6162610
jsirianni <jsirianni@ws1.sir.local>
2017-09-12 20:42:04 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'GPL'
 depends          'team-base'
 description      'Installs/Configures a team miner'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
