e610f644644ba01bf8b1f508af436619ae18a8b8
jsirianni <jsirianni@ws1.sir.local>
2017-11-29 21:28:53 -0500

    disable rsyslog unless set to true

--- recipes/syslog.rb
+++ recipes/syslog.rb
@@ -1,14 +1,20 @@
-# Place syslog conf
-template "/etc/rsyslog.d/60-syslog.conf" do
-  user     'root'
-  group    'root'
-  source   '60-syslog.conf.erb'
-  mode     '0644'
-  notifies :restart, 'service[rsyslog]'
-  action   :create
-end
+if node[:miner][:enablesyslog] != false
+      # Place syslog conf
+      template "/etc/rsyslog.d/60-syslog.conf" do
+        user     'root'
+        group    'root'
+        source   '60-syslog.conf.erb'
+        mode     '0644'
+        notifies :restart, 'service[rsyslog]'
+        action   :create
+      end
 
-# Restart rsyslog if template updated
-service 'rsyslog' do
-      action :nothing
+      # Restart rsyslog if template updated
+      service 'rsyslog' do
+            action :nothing
+      end
+else
+      service 'rsyslog' do
+            action :stop
+      end
 end
